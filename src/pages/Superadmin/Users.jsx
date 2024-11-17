513b2bf75731cd2c7c0c736032bd1f4148d21e5c
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2024-11-17 23:12:55 +0700

    fix design

--- src/pages/Superadmin/Users.jsx
+++ src/pages/Superadmin/Users.jsx
@@ -40,7 +40,7 @@ const UserList = () => {
   }, []);
 
   return (
-    <div className="max-w-5xl mx-auto p-4 bg-white rounded-lg">
+    <div className="max-w-6xl mx-auto p-4 bg-white rounded-lg">
       <div className="bg-white p-4 rounded-lg">
         {loading && <Loading />}
         {!loading && (
