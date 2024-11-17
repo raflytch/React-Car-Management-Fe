7724a6e61df1d58c3595fe26f77aad660998a4db
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2024-11-18 00:12:20 +0700

    add sidebar

---
+++ src/components/Fragments/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthRole } from "../../contexts/AuthRoleContext";
+
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+  const navigate = useNavigate();
+  const { user, logout } = useAuthRole();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <div
+      className={`fixed top-0 left-0 h-full bg-slate-200 text-gray-800 z-40 transition-transform ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } lg:translate-x-0 w-64`}
+    >
+      <div className="flex flex-col h-full">
+        <div className="p-4 border-b border-gray-300 flex items-center justify-end">
+          <h1 className="text-sm font-bold">Dashboard</h1>
+        </div>
+        <nav className="flex-1 p-4">
+          <ul className="space-y-4">
+            <li>
+              <Link
+                to="/dashboard/users"
+                className="block py-2 px-4 rounded-lg hover:bg-gray-200"
+              >
+                Users
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/dashboard/cars"
+                className="block py-2 px-4 rounded-lg hover:bg-gray-200"
+              >
+                Cars
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="p-4 border-t border-gray-300">
+          <div className="flex items-center space-x-4 mb-4">
+            <img
+              src={user?.fotoProfil || "/default-profile.png"}
+              alt={`${user?.firstName} ${user?.lastName}`}
+              className="w-12 h-12 rounded-full object-cover border border-gray-400"
+            />
+            <div>
+              <p className="text-gray-800 font-semibold">
+                {user?.email.split("@")[0]}
+              </p>
+            </div>
+          </div>
+          <button
+            onClick={handleLogout}
+            className="w-full py-2 px-4 bg-red-500 rounded-lg text-white hover:bg-red-600"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
