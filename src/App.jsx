e41bd4b5e9c1a4eec7d8dcc9f99bf9b6ae2bfe7d
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-11-17 20:58:00 +0700

    add users/superadmin

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Superadmin/Dashboard";
 import { AuthRoleProvider } from "./contexts/AuthRoleContext";
 import UpdateCar from "./pages/Superadmin/UpdateCar";
 import NotFoundPage from "./pages/NotFoundPage";
+import UserList from "./pages/Superadmin/Users";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="update-car/:id" element={<UpdateCar />} />
+            <Route path="users" element={<UserList />} />
           </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
