05eec7200c80955b2eca969d52ba04228496dae7
TegarAlfaR <tegaralfa18052004@gmail.com>
2024-11-17 15:05:23 +0700

    feat: redesign update car page

--- src/pages/Superadmin/UpdateCar.jsx
+++ src/pages/Superadmin/UpdateCar.jsx
@@ -1,96 +1,83 @@
 import React from "react";
-import ProtectedRoute from "../../components/ProtectedRoute";
 
 const UpdateCar = () => {
-  const products = [
-    {
-      name: 'Apple MacBook Pro 17"',
-      color: "Silver",
-      category: "Laptop",
-      price: "$2999",
-    },
-    {
-      name: "Microsoft Surface Pro",
-      color: "White",
-      category: "Laptop PC",
-      price: "$1999",
-    },
-    {
-      name: "Magic Mouse 2",
-      color: "Black",
-      category: "Accessories",
-      price: "$99",
-    },
-    {
-      name: "Google Pixel Phone",
-      color: "Gray",
-      category: "Phone",
-      price: "$799",
-    },
-    {
-      name: "Apple Watch 5",
-      color: "Red",
-      category: "Wearables",
-      price: "$999",
-    },
-  ];
-
   return (
-    <ProtectedRoute>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
+    <section className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-2xl px-4 py-16 sm:px-6">
+        <div className="rounded-lg bg-white p-8 shadow-lg">
+          <form action="#" className="space-y-4">
+            <div>
+              <label className="" htmlFor="carName">
+                Car Name
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Car Name"
+                type="text"
+                id="carName"
+              />
+            </div>
+
+            <div>
+              <label className="" htmlFor="year">
+                Year
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Year"
+                type="text"
+                id="year"
+              />
+            </div>
+
+            <div>
+              <label className="" htmlFor="licensePlate">
+                License Plate
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="License Plate"
+                type="text"
+                id="licensePlate"
+              />
+            </div>
+
+            <div>
+              <label className="" htmlFor="price">
                 Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product, index) => (
-              <tr
-                key={index}
-                className={`${
-                  index % 2 === 0
-                    ? "bg-white dark:bg-gray-900"
-                    : "bg-gray-50 dark:bg-gray-800"
-                } border-b dark:border-gray-700`}
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="price"
+                type="text"
+                id="price"
+              />
+            </div>
+
+            <div>
+              <label className="" htmlFor="carImage">
+                Car Image
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Car Image"
+                type="file"
+                id="carName"
+              />
+            </div>
+
+            <div className="mt-4">
+              <button
+                type="submit"
+                className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
               >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {product.name}
-                </th>
-                <td className="px-6 py-4">{product.color}</td>
-                <td className="px-6 py-4">{product.category}</td>
-                <td className="px-6 py-4">{product.price}</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </ProtectedRoute>
+    </section>
   );
 };
 
