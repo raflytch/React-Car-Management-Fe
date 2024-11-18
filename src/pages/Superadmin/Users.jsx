58244e17168504c3256c3afd0957a364142c340c
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2024-11-19 02:09:30 +0700

    fix users

--- src/pages/Superadmin/Users.jsx
+++ src/pages/Superadmin/Users.jsx
@@ -26,7 +26,11 @@ const UserList = () => {
   return (
     <div className="max-w-6xl mx-auto p-4 bg-white rounded-lg shadow-md">
       <div className="bg-white p-6 rounded-lg shadow-sm">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">User List</h1>
+        {loading ? (
+          <Skeleton height={48} width={150} className="mb-6" />
+        ) : (
+          <h1 className="text-3xl font-bold text-gray-800 mb-6">User List</h1>
+        )}
 
         {loading ? (
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-6">
