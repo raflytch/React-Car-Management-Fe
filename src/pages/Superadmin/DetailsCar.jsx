dff99f866da158f7c47bfdbf1064f9b9258c3a9d
JetroSulthan <jetrosulthan9626@gmail.com>
2024-11-19 15:04:14 +0700

    fix: feat details-car

--- src/pages/Superadmin/DetailsCar.jsx
+++ src/pages/Superadmin/DetailsCar.jsx
@@ -3,12 +3,23 @@ import Navbar from "../../components/Fragments/Navbar";
 import Footer from "../../components/Fragments/Footer";
 import ProtectedRoute from "../../components/ProtectedRoute";
 import useProtectedAll from "../../hooks/useProtectedAll";
+import Button from "../../components/Elements/Buttons/Button";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DetailsCar = () => {
     useProtectedAll(["admin", "superadmin"])
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    const handleToUpdate = (id) => navigate(`/update-car/${id}`)
+
     return (
         <div>
             <CarDetail />
+            <div>
+                <Button onAction={handleToUpdate}>Edit</Button>
+
+            </div>
         </div>
     )
 }
